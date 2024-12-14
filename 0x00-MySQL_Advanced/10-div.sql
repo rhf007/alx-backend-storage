b3b49a9298484b58bdbf9d300945f0d3918732bc
Rahaf Hazem <110139745+rhf007@users.noreply.github.com>
2024-12-14 15:15:31 +0200

    Create 10-div.sql

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- performs a safe division of a by b, returning 0 if b is 0 to avoid a divide by zero error.
+
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER $$
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    DECLARE result FLOAT DEFAULT 0;
+
+    IF b != 0 THEN
+        SET result = a / b;
+    END IF;
+    RETURN result;
+END $$
+DELIMITER ;
