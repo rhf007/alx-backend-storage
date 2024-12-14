74d56266919b935087e1c0c2b41515f851d90f74
Rahaf Hazem <110139745+rhf007@users.noreply.github.com>
2024-12-14 14:29:28 +0200

    Create 1-country_users.sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- create users table with
+-- id, email, name, country(enum of US, CO and TN)
+CREATE TABLE IF NOT EXISTS users(
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
