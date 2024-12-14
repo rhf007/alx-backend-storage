9d2b39303c9c0657991a232b124e00ab4bfcc493
Rahaf Hazem <110139745+rhf007@users.noreply.github.com>
2024-12-14 16:24:56 +0200

    Create 101-students.py

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''
+Top students
+'''
+
+
+def top_students(mongo_collection):
+    '''
+    prints all students sorted by average score.
+    '''
+    std = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return std
