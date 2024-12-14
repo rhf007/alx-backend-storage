58088eb4580092ceb9b450b273968d9726f20607
Rahaf Hazem <110139745+rhf007@users.noreply.github.com>
2024-12-14 15:16:28 +0200

    Create 11-need_meeting.sql

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,4 @@
+-- list of all students with score under 80 and no last_meeting or more than 1 month
+CREATE OR REPLACE VIEW need_meeting AS SELECT name FROM students
+WHERE score < 80 AND (last_meeting IS NULL OR
+TIMESTAMPDIFF(MONTH, last_meeting, CURDATE()) > 1);
